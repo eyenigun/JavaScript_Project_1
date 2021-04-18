07714d2ffc85713b74c813d1396c2787119ff4b2
Erhan Yenigun <eyenigun@gmail.com>
2021-04-18 12:42:46 -0400

    init

--- date_time.js
+++ date_time.js
@@ -1,6 +1,6 @@
 // program to display the date and time
 // get date and time
-const date = new Date(2017, 2, 12, 9, 25, 30);
+const date = new Date(2018, 2, 12, 9, 25, 30);
 
 // get the date as a string
 const n = date.toDateString();
